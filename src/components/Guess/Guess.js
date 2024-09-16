d435e7bdb1f90efbf7b617f1d8a8541531b2db3e
John Ritterbush <jritterbush@gmail.com>
2024-09-16 18:37:30 -0500

    simplify again

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -2,17 +2,22 @@ import React from "react";
 import { range } from "../../utils";
 import { checkGuess } from "../../game-helpers";
 
+function Cell({ letter, status }) {
+  const classes = status ? `cell ${status}` : "cell";
+  return <span className={classes}>{letter}</span>;
+}
+
 function Guess({ answer, word }) {
   const checkedWord = word ? checkGuess(word, answer) : [];
   return (
     <p className="guess">
-      {checkedWord.length > 0
-        ? checkedWord.map(({ letter, status }, index) => (
-            <span key={index} className={`cell ${status}`}>
-              {letter}
-            </span>
-          ))
-        : range(5).map((number) => <span key={number} className="cell"></span>)}
+      {range(5).map((number) => (
+        <Cell
+          key={number}
+          letter={checkedWord[number] ? checkedWord[number].letter : undefined}
+          status={checkedWord[number] ? checkedWord[number].status : undefined}
+        />
+      ))}
     </p>
   );
 }
